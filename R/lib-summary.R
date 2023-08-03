3a28a3ac92c465b8f111adb7a3aaa898a9b675e4
Chris Madsen <chris.madsen@gov.bc.ca>
2023-08-03 11:58:28 -0700

    added roxygen documentation to lib_summary function

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,3 +1,15 @@
+#' Enumerate installed R packages
+#'
+#' Searches for and enumerates R packages installed in various locations on your local machine.
+#' It takes no arguments.
+#'
+#' @return A data.frame containing the count of packages
+#'          in each of your libraries.
+#' @export
+#'
+#' @examples
+#' all_pkgs <- lib_summary()
+#' all_pkgs
 lib_summary <- function() {
   # Check installed packages on machine.
   pkgs <- utils::installed.packages()
