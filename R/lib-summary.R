f8ec4116c2c8e123b47aa71e99ffec937bc9dd36
Madsen <Chris.Madsen@gov.bc.ca>
2023-08-03 16:27:41 -0700

    Split out function for sizes function

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -26,15 +26,37 @@ lib_summary <- function(sizes = FALSE) {
   # Improve column names.
   names(pkg_df) <- c("library","n_packages")
 
+  # if (sizes) {
+  #   pkg_df$file_size = map_dbl(
+  #     pkg_df$library,
+  #     ~ sum(fs::file_size(fs::dir_ls(.x, recurse = TRUE)))
+  #   )
+  #
+  #   pkg_df$file_size_mb = paste0(round(pkg_df$file_size / 1000000,2), ' MB')
+  # }
   if (sizes) {
-    pkg_df$file_size = map_dbl(
-      pkg_df$library,
-      ~ sum(fs::file_size(fs::dir_ls(.x, recurse = TRUE)))
-    )
-
-    pkg_df$file_size_mb = paste0(round(pkg_df$file_size / 1000000,2), ' MB')
+    pkg_df <- calculate_sizes(pkg_df)
   }
-
   # return the results
   pkg_df
+}
+
+#' Calculate Sizes
+#'
+#' @param df a data.frame
+#'
+#' @return df with file_size and file_size_mb columns added
+#'
+#' @examples
+#' @noRd
+calculate_sizes = function(df){
+
+  df$file_size = map_dbl(
+    df$library,
+    ~ sum(fs::file_size(fs::dir_ls(.x, recurse = TRUE)))
+  )
+
+  df$file_size_mb = paste0(round(df$file_size / 1000000,2), ' MB')
+
+  df
 }
